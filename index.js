8a7f30a2718c156a8bf82f854ee300269d158003
Rabiullah-Rabi <mdrabiullah789@gmail.com>
2022-10-26 13:52:35 +0600

    serve id wise courses

--- index.js
+++ index.js
@@ -5,17 +5,26 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 const corurseCategories = require('./data/courseCat/courseCat.json');
-const course = require('./data/courseDetails/courseDetails.json');
+const courses = require('./data/courseDetails/courseDetails.json');
 app.get("/", (req, res) => {
   res.send("api running");
 });
 app.get("/category", (req, res) => {
     res.send(corurseCategories);
 });
-app.get("/course", (req, res) => {
-    res.send(course);
+app.get("/category/:id",(req, res) => {
+    const id = req.params.id;
+    const selectedCategory = courses.filter((category) => category.catID === id);
+    res.send(selectedCategory);
+  });
+app.get("/courses", (req, res) => {
+    res.send(courses);
 });
-
+app.get("/course/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedCourses = courses.find((course) => course.id === id);
+    res.send(selectedCourses);
+  });
 app.listen(port, () => {
   console.log("port is ", port);
 });
