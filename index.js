0543833ae954b7327027070f5a09b74a1c1f66f6
Rabiullah-Rabi <mdrabiullah789@gmail.com>
2022-10-26 12:47:24 +0600

    response to course cat and course aoi call

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-
+const corurseCategories = require('./data/courseCat/courseCat.json');
+const course = require('./data/courseDetails/courseDetails.json');
 app.get("/", (req, res) => {
   res.send("api running");
+});
+app.get("/category", (req, res) => {
+    res.send(corurseCategories);
+});
+app.get("/course", (req, res) => {
+    res.send(course);
 });
 
 app.listen(port, () => {
